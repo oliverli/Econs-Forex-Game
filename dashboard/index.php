2101309b464c15e7ea13c5d838de8e90e929c188
walltail <wwwwaltwalter@gmail.com>
2015-12-28 23:22:45 +0800

    Revert "Final materialize commit"
    This reverts commit 6b2a685c7a44e70bed613e2a18edfadd7a7b43d4.

--- dashboard/index.php
+++ dashboard/index.php
@@ -100,45 +100,14 @@
             </div>-->
         </nav>
         <div class="container">
-            <div class="card"><div class="card-content center"><?php
-                    $userkey = intval($_SESSION["userkey"]);
-                    if($userkey <= 0)
-                        return;
-                    $query = "SELECT name, networth FROM users WHERE userkey=$userkey LIMIT 1";
-                    $result = $db->query($query) or die();
-                    $totalvalue = 0;
-                    if($row = $result->fetch_assoc())
-                    {
-                        echo "<p>Hello, ".$row["name"].".";
-                        $totalvalue = $row["networth"];
-                    }
-                    if(!$gameEnded)
-                        echo " Your net worth is ".$basecurrabbv.number_format($totalvalue, 2).".</p>";
-                    else
-                    {
-                        echo " The game has ended.</p><p>";
-                        if($totalvalue == 10000000)
-                        {
-                            echo " You did not make or lose any money.</p>";
-                        }
-                        else if($totalvalue > 10000000)
-                        {
-                            echo " You made a profit of ".$basecurrabbv.number_format($totalvalue - 10000000, 2)."!</p>";
-                        }
-                        else
-                        {
-                            echo " You lost ".$basecurrabbv.number_format(0 - ($totalvalue - 10000000), 2).".</p>";
-                        }
-                    }
-                ?></div></div>
-            <div id="board" class="card">
-                <table style="width:100%" class="card-content">
-                    <tr class="indigo lighten-2" >
-                        <th class="center">Currency</th>
-                        <th class="center">USD Selling Value</th>
-                        <th class="center">USD Buying Value</th>
-                        <th class="center">Amount Owned</th>
-                        <th class="center">Buy/Sell</th>
+            <div id="board" style="">
+                <table style="width:100%">
+                    <tr class="red">
+                        <th class="red">Currency</th>
+                        <th class="red">USD Selling Value</th>
+                        <th class="red">USD Buying Value</th>
+                        <th class="red">Amount Owned</th>
+                        <th class="red">Buy/Sell</th>
                     </tr>
                     <?php
                         $query = "SELECT amount FROM wallet WHERE currencyid=1 AND userkey = $userkey LIMIT 1";
@@ -146,11 +115,11 @@
                         while($row = $result->fetch_assoc())
                         {
                             echo "<tr>";
-                            echo "<td class=\"center\">$basecurrname ($basecurrabbv)</td>";
-                            echo "<td class=\"center\">N.A.</td>";
-                            echo "<td class=\"center\">N.A.</td>";
-                            echo "<td class=\"center\">".number_format($row["amount"], 2)."</td>";
-                            echo "<td class=\"center\">N.A.</td>";
+                            echo "<td class=\"table-bordered\">$basecurrname ($basecurrabbv)</td>";
+                            echo "<td class=\"table-bordered\">N.A.</td>";
+                            echo "<td class=\"table-bordered\">N.A.</td>";
+                            echo "<td class=\"table-bordered\">".number_format($row["amount"], 2)."</td>";
+                            echo "<td class=\"table-bordered\">N.A.</td>";
                             echo "</tr>";
                             break;
                         }
@@ -159,32 +128,32 @@
                         while($row = $result->fetch_assoc())
                         {
                             echo "<tr>";
-                            echo "<td class=\"center\">".$row["name"]." (";
+                            echo "<td class=\"table-bordered\">".$row["name"]." (";
                             echo $row["shortname"].")</td>";
-                            echo "<td class=\"center\">".$row["buyvalue"]."</td>";
-                            echo "<td class=\"center\">".$row["sellvalue"]."</td>";
-                            echo "<td class=\"center\">".number_format($row["amount"], 2)."</td>";
-                            echo "<td class=\"center\"><a href='./buysell/?currid=".$row["currencyid"]."' target=\"_top\" data-ftrans=\"slide\" id=\"buysell\">Buy/Sell</a></td>";
+                            echo "<td class=\"table-bordered\">".$row["buyvalue"]."</td>";
+                            echo "<td class=\"table-bordered\">".$row["sellvalue"]."</td>";
+                            echo "<td class=\"table-bordered\">".number_format($row["amount"], 2)."</td>";
+                            echo "<td class=\"table-bordered\"><a href='./buysell/?currid=".$row["currencyid"]."' target=\"_top\" data-ftrans=\"slide\" id=\"buysell\">Buy/Sell</a></td>";
                             echo "</tr>";
                         }
                     ?>
                 </table>
             </div>
-            <div id="news" class="card">
-                <table style="width:100%" class="card-content center">
-                    <tr class="indigo lighten-2">
-                        <th colspan="2" class="center">BBT News Headlines</th>
+            <div id="news" style="position:relative; top:10px;">
+                <table style="width:100%">
+                    <tr class="red">
+                        <th colspan="2" class="red">BBT News Headlines</th>
                     </tr>
                     <?php
                         $query = "SELECT newstext, time FROM news WHERE time <= ".time()." ORDER BY time DESC LIMIT 30";
                         $result = $db->query($query) or die($db->error);
                         if($result->num_rows <= 0)
-                            echo "<tr><td colspan=\"2\" class=\"center\">There are no news reports at the moment.</td></tr>";
+                            echo "<tr><td colspan=\"2\" class=\"table-bordered\">There are no news reports at the moment.</td></tr>";
                         while($row = $result->fetch_assoc())
                         {
                             echo "<tr>";
-                            echo "<td class=\"center\">".$row["newstext"]."</td>";
-                            echo "<td class=\"center\" style=\"width:25%\">".nicetime($row["time"])."</td>";
+                            echo "<td class=\"table-bordered\">".$row["newstext"]."</td>";
+                            echo "<td class=\"table-bordered\" style=\"width:25%\">".nicetime($row["time"])."</td>";
                             echo "</tr>";
                         }
                     ?>
